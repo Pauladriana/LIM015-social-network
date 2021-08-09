549f25736bdc60733675a63f1209be1741bd405c
Claudi4 <clau.ale.20a@gmail.com>
2021-08-09 00:07:20 -0500

    cambios ultimos de main

--- src/logingroup.js
+++ src/logingroup.js
@@ -1,13 +1,66 @@
-import {
-  createLogin,
-} from './vistas/login.js';
+const secciones = document.querySelector("#secciones");
 
-const secciones = document.querySelector('#secciones');
-
-// CREANDO DINAMICAMENTE EL VISTA LOGIN
+// creando dinamicamente la vista login (href = muro)
+export const createLogin = `
+<form action="" id="login-form">
+  <input type="email" placeholder="email" class="login-input" id="login-email" required>
+  <input type="password" placeholder="password" class="login-input" id="login-password" required>
+  <div class="mostrar">
+      <input type="checkbox" class="show-password" id="show-password">
+      <label for="accept" >Mostrar contraseña</label>
+  </div>
+  <button type="button" id="login-button" class="submit-button">LOGIN</button>
+</form>
+<p>OR</p>
+<button type="button" id="google-login" class="google-login">
+  <img src="./imagen/google.png" class="imgGoogle"/>
+  Log in with  google
+</button>
+<p>Dont have an account?<a href="#signup" id="signingup">Sign up</a></p> 
+<a href="#">Forgot my password</a>`;
 secciones.innerHTML = createLogin;
 
+// creando dinamicamente la vista registro (href = login)
+export const createSignup = `
+  <p>Travel with me</p>
+  <form action="" id="signup-form">
+    <input type="text" placeholder="Fullname" class="login-input" id="fullname" required>
+    <input type="text" placeholder="Username" class="login-input" id="username" required>
+    <input type="password" placeholder="Password" class="login-input"  id="signup-password" required minlength="6" maxlength="12">
+    <p id="shortPassword"></p>
+    <input type="password" placeholder="Confirm password" class="login-input" required>
+    <input type="email" placeholder="Email" class="login-input" id="signup-email" required>
+    <div class="terminos">
+      <input type="checkbox" class="accept" id="accept">
+      <label for="accept">He Leido y acepto los terminos y condiciones</label>
+    </div>
+    <button type="button" id="submit-button" class="submit-button">SIGN UP</button>
+  </form>
+  <button class="cancel-button" id="cancel-button">Cancel</button>`;
 
-  /* logout - cerrar sesion */
-  
-};
+// creando dinamicamente la vista muro (href = login)
+export const createMuro = `
+    <p>LOGRASTE INGRESAR: BIENVENIDO A TRAVELER.PE</p>
+    <p>Estos son los traveleros:</p>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 offset-md-3">
+          <ul class="list-group" id="userslist">
+            
+          </ul>
+
+        </div>
+      </div>
+    </div>
+    <button class="logout-button" type="button" id="logout-button">LOGOUT</button>`;
+
+// icono para mostrar contraseña
+const showPassword = document.querySelector("#show-password");
+showPassword.addEventListener("change", () => {
+  const password1 = document.querySelector("#login-password");
+  if (password1.type === "text") {
+    password1.type = "password";
+  } else {
+    password1.type = "text";
+  }
+});
--- src/main.js
+++ src/main.js
@@ -1,91 +1,294 @@
-import {
-  createLogin, createSignup, createMuro, usuariosMuro,
-} from './logingroup.js';//ARREGLAR
+import {createLogin, createSignup, createMuro} from './logingroup.js';
 
-// ENROUTAMIENTO
+//ENROUTAMIENTO codigo bonito
 const secciones = document.querySelector('#secciones');
 
-// EVITANDO 404
+//EVITANDO 404
 console.log(window.location.pathname);
-if (window.location.pathname === '/login') {
+if(window.location.pathname === '/login'){
   secciones.innerHTML = createLogin;
-} else if (window.location.pathname === '/signup') {
+} else if (window.location.pathname === '/signup'){
   secciones.innerHTML = createSignup;
-} else if (window.location.pathname === '/muro') {
+} else if (window.location.pathname === '/muro'){
   secciones.innerHTML = createMuro;
-  usuariosMuro();
+  const allUsers = document.querySelector("#userslist");
+
+      const setupUsers = (data) => {
+        if (data.length) {
+          let html = "";
+          data.forEach((doc) => {
+            const user = doc.data();
+            console.log(user);
+            const li = `
+                      <li class='list-group-item list-group-item-action'>
+                          <h5>${user.username}</h5>
+                          <p>${user.fullname}</p>
+                      </li>`;
+            html += li;
+          });
+          allUsers.innerHTML = html;
+        } else {
+          allUsers.innerHTML = `<p>Login to meet the travelers</p>`;
+        }
+      };
+
+    // Eventos
+    // Listar los datos para usuarios autenticados
+      auth.onAuthStateChanged((user) => {
+        if (user) {
+          fs.collection("users")
+            .get()
+            .then((snapshot) => {
+              setupUsers(snapshot.docs);
+            });
+        } else {
+          setupUsers([]);
+        }
+      });
+      /* logout - cerrar sesion */
+      const logout = document.querySelector("#logout-button");
+      logout.addEventListener("click", (e) => {
+        e.preventDefault();
+        auth.signOut().then(() => {
+          console.log("cerraste sesion");
+          window.history.pushState( {} , 'muro', '/login' );
+          secciones.innerHTML = createLogin;
+
+        });
+      });
+
+  
 }
 
-// RUTA SIN #
+//RUTA SIN #
 const changeRoute = (hash) => {
-  if (hash === '#login') {
-    window.history.replaceState({}, 'login', '/login');
-  } else if (hash === '#signup') {
-    window.history.replaceState({}, 'signup', '/signup');
-  } else if (hash === '#muro') {
-    window.history.replaceState({}, 'muro', '/muro');
+  if (hash === '#login'){
+    window.history.replaceState({}, 'login', '/login')
+  } else if (hash === '#signup'){
+    window.history.replaceState({}, 'signup', '/signup')
+  } else if (hash === '#muro'){
+    window.history.replaceState({}, 'muro', '/muro')
   }
 };
 
 window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#signup') {
+    if (window.location.hash === '#signup') {
     console.log('mostrar registro');
     secciones.innerHTML = createSignup;
-    changeRoute(window.location.hash);
-    //AGREGAR LO DE AUTH(LOGIN)
-  } else if (window.location.hash === '#login') {
-    console.log('mostrar login');
-    secciones.innerHTML = createLogin;//CAMBIAR
-    changeRoute(window.location.hash);
-  } else if (window.location.hash === '#muro') {
-    changeRoute(window.location.hash);
-    secciones.innerHTML = createMuro;//CAMBIAR?
-  }
+
+    changeRoute(window.location.hash)
+
+    // limitar contraseña
+    const contraseña = document.querySelector("#signup-password");
+    const mensajePassword = document.querySelector("#shortPassword");
+    contraseña.addEventListener( "change" , () => {
+      if (contraseña.value.length < 6) {
+        mensajePassword.innerHTML = "Tu contraseña debe tener al menos 6 caracteres";
+       mensajePassword.style.color = "red";
+     } else {
+      mensajePassword.style.display = "none";
+     }
+    })
+    //fin
+
+    const signupForm = document.querySelector("#signup-form");
+    const botonForm = document.querySelector("#submit-button");
+
+    
+
+    botonForm.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("registrandote");
+    const signupEmail = document.querySelector("#signup-email").value;
+    const signupPassword = document.querySelector("#signup-password").value;
+    const usernameInput = document.querySelector("#username").value;
+    const fullnameInput = document.querySelector("#fullname").value;
+    const passwordInput = document.querySelector('#signup-password').value;
+    const emailInput = document.querySelector('#signup-email').value;
+
+    auth
+    .createUserWithEmailAndPassword(signupEmail, signupPassword)
+    .then((userCredential) => {
+      console.log("registrado");
+      fs.collection("users").add({
+        username: usernameInput,
+        fullname: fullnameInput,
+        password: passwordInput,
+        email: emailInput
+      })
+      .then((docRef) => {
+        console.log("Este es el nuevo usuario: " + docRef.id);
+      })
+      .catch((error) => {
+        console.log("Tienes el siguiente error: " + error);
+      })
+      signupForm.reset();
+      window.history.pushState( {} , 'signup', '/login' );
+      secciones.innerHTML = createLogin;// no deberia se asi, casi nos morimos f
+    })
+
+  })
+} else if (window.location.hash === '#login') {
+  console.log('mostrar login');
+  secciones.innerHTML = createLogin;
+  changeRoute(window.location.hash)
+} else if (window.location.hash === '#muro'){
+  changeRoute(window.location.hash);
+  secciones.innerHTML = createMuro;
+}
+
 });
 
-// FLECHAS DE ATRAS Y ADELANTE ------> NO FUNCIONA!
-/* window.onpopstate( () => {
+//FLECHAS DE ATRAS Y ADELANTE ------> NO FUNCIONA!
+/*window.onpopstate( () => {
   if(window.location.pathname === '/login'){
     secciones.innerHTML = createLogin;
     console.log(' LOGIN')
   } else if (window.location.pathname === '/signup'){
     secciones.innerHTML = createSignup;
     console.log(' REGISTRO')
-  }
+  }  
 }); */
 
-// LOGIN- LOGEARSE
-const loginForm = document.querySelector('#login-form');
+// login - logearse
+const loginForm = document.querySelector("#login-form");
 const loginButon = document.querySelector('#login-button');
 
-loginButon.addEventListener('click', (e) => {
+loginButon.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log('logueandote');
-  const loginEmail = document.querySelector('#login-email').value;
-  const loginPassword = document.querySelector('#login-password').value;
+  console.log("logueandote");
+  const loginEmail = document.querySelector("#login-email").value;
+  const loginPassword = document.querySelector("#login-password").value;
   console.log(loginEmail, loginPassword);
 
   auth
     .signInWithEmailAndPassword(loginEmail, loginPassword)
     .then((userCredential) => {
-      console.log('logueado');
+      console.log("logueado");
       loginForm.reset();
-      window.history.pushState({}, 'login', '/muro');
+      window.history.pushState( {} , 'login', '/muro' );
       secciones.innerHTML = createMuro;
-      usuariosMuro();
+      /* usuarios - lista en el muro */
+      const allUsers = document.querySelector("#userslist");
+
+      const setupUsers = (data) => {
+        if (data.length) {
+          let html = "";
+          data.forEach((doc) => {
+            const user = doc.data();
+            console.log(user);
+            const li = `
+                      <li class='list-group-item list-group-item-action'>
+                          <h5>${user.username}</h5>
+                          <p>${user.fullname}</p>
+                      </li>`;
+            html += li;
+          });
+          allUsers.innerHTML = html;
+        } else {
+          allUsers.innerHTML = `<p>Login to meet the travelers</p>`;
+        }
+      };
+
+      // Eventos
+      // Listar los datos para usuarios autenticados
+      auth.onAuthStateChanged((user) => {
+        if (user) {
+          fs.collection("users")
+            .get()
+            .then((snapshot) => {
+              setupUsers(snapshot.docs);
+            });
+        } else {
+          setupUsers([]);
+        }
+      });
+      /* logout - cerrar sesion */
+      const logout = document.querySelector("#logout-button");
+      logout.addEventListener("click", (e) => {
+        e.preventDefault();
+        auth.signOut().then(() => {
+          console.log("cerraste sesion");
+          window.history.pushState( {} , 'muro', '/login' );
+          secciones.innerHTML = createLogin;
+
+        });
+      });
+  
     });
 });
 
-// LOGUEARSE CON GOOGLE
-const googleButton = document.querySelector('#google-login');
-googleButton.addEventListener('click', (e) => {
+// login with google - logearse con google hola
+const googleButton = document.querySelector("#google-login");
+googleButton.addEventListener("click", (e) => {
+  e.preventDefault();
   const provider = new firebase.auth.GoogleAuthProvider();
   auth
     .signInWithPopup(provider)
     .then((result) => {
-      console.log('te logueaste con google');
+      console.log("te logueaste con google");
       loginForm.reset();
+      window.history.pushState( {} , 'login', '/muro' );
       secciones.innerHTML = createMuro;
-      usuariosMuro();
+      /* usuarios - lista en el muro */
+      const allUsers = document.querySelector("#userslist");
+
+      const setupUsers = (data) => {
+        if (data.length) {
+          let html = "";
+          data.forEach((doc) => {
+            const user = doc.data();
+            console.log(user);
+            const li = `
+                      <li class='list-group-item list-group-item-action'>
+                          <h5>${user.username}</h5>
+                          <p>${user.fullname}</p>
+                      </li>`;
+            html += li;
+          });
+          allUsers.innerHTML = html;
+        } else {
+          allUsers.innerHTML = `<p>Login to meet the travelers</p>`;
+        }
+      };
+
+    // Eventos
+    // Listar los datos para usuarios autenticados
+      auth.onAuthStateChanged((user) => {
+        if (user) {
+          fs.collection("users")
+            .get()
+            .then((snapshot) => {
+              setupUsers(snapshot.docs);
+            });
+        } else {
+          setupUsers([]);
+        }
+      });
+      /* logout - cerrar sesion */
+      const logout = document.querySelector("#logout-button");
+      logout.addEventListener("click", (e) => {
+        e.preventDefault();
+        auth.signOut().then(() => {
+          console.log("cerraste sesion");
+          window.history.pushState( {} , 'muro', '/login' );
+          secciones.innerHTML = createLogin;
+
+        });
+      });
+
+    })
+    .catch((err) => {
+      console.log(err);
     });
 });
+
+
+
+
+
+
+
+
+
+
