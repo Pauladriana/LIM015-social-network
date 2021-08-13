553308009fe0343177c5e929fbe3bf87cdfb9054
Pauladriana <85115054+Pauladriana@users.noreply.github.com>
2021-08-13 18:03:44 -0500

    Se añadieron test y funcionan

--- src/firebase.js
+++ src/firebase.js
@@ -4,27 +4,25 @@ import {emailUserRegister} from './login.js';
 export const crearUsuarioFb = (signupEmail, signupPassword, usernameInput, fullnameInput, passwordInput, emailInput) => {
   emailUserRegister(signupEmail, signupPassword)
     .then((userCredential) => {
-      console.log("registrado");
+      // console.log("registrado");
       fs.collection("users").add({
         username: usernameInput,
         fullname: fullnameInput,
         password: passwordInput,
         email: emailInput
       })
-      .then((docRef) => {
-        console.log("Este es el nuevo usuario: " + docRef.id);
-      })
-      .catch((error) => {
-        console.log("Tienes el siguiente error: " + error);
-      })
-      
+        .then((docRef) => {
+        // console.log("Este es el nuevo usuario: " + docRef.id);
+        })
+        .catch((error) => {
+          // console.log("Tienes el siguiente error: " + error);
+        });
     })
     .catch((err) => {
-      const wrongSsignupEmail = document.querySelector('#wrongSUemail');
-        if (err.message == 'The email address is already in use by another account.'){
-          wrongSignupEmail.innerHTML = 'Este correo ya esta en uso, intenta con otro';
-          wrongSignupEmail.style.color = 'red'
-        }
-      })
+      const wrongSignupEmail = document.querySelector('#wrongSUemail');
+      if (err.message === 'The email address is already in use by another account.') {
+        wrongSignupEmail.innerHTML = 'Este correo ya esta en uso, intenta con otro';
+        wrongSignupEmail.style.color = 'red';
+      }
+    });
 };
-
